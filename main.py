c0647c544f5ac0b218f365d3dc2a8c17eb881d95
Tonio <toha.futbolist@gmail.com>
2023-02-24 15:23:05 +1000

    Authentication routers

--- main.py
+++ main.py
@@ -6,11 +6,40 @@ from fastapi.responses import JSONResponse
 from datetime import datetime
 from enum import Enum
 from typing import List, Optional
+
+from fastapi_users import FastAPIUsers
+from auth.auth import auth_backend
+from auth.database import User
+from auth.manager import get_user_manager
+
 from pydantic import BaseModel, Field
+
+from auth.schemas import UserRead, UserCreate
 
 app = FastAPI(
     title="Trading App"
 )
+
+fastapi_users = FastAPIUsers[User, int](
+    get_user_manager,
+    [auth_backend],
+)
+
+app.include_router(
+    fastapi_users.get_auth_router(auth_backend),
+    prefix="/auth/jwt",
+    tags=["auth"],
+)
+
+app.include_router(
+    fastapi_users.get_register_router(UserRead, UserCreate),
+    prefix="/auth",
+    tags=["auth"],
+)
+
+current_user = fastapi_users.current_user()
+
+
 
 
 @app.exception_handler(ValidationError)
